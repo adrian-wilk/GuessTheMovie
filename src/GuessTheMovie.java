32b478b395f552522186d5a94e65a817c8e73bb0
Adrian Wilk <awilk2@gmail.com>
2020-04-08 16:29:26 +0200

    GuessTheMovie class added and movies.txt added. GuessTheMovie able to count number of films from movies.txt.

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+import java.io.File;
+
+
 public class GuessTheMovie {
+
+public static void main(String[] args) throws Exception {
+
+    // Guessing the title should end the game. Initial isCorrect value set to false.
+    boolean isCorrect = false;
+
+    File movieTitle = new File("movies.txt");
+    Scanner movieScanner = new Scanner(movieTitle);
+
+    // The movies file may be changed and new titles may be added. Therefore let's count how many movies is there before we proceed.
+    int titlesNumber=0;
+    while(movieScanner.hasNextLine()){
+        String line = movieScanner.nextLine();
+        titlesNumber += (line.split("\n").length);
+    }
+System.out.println(titlesNumber);
+
+
+
+}
+
 }
